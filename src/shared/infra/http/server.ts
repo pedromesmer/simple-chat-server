1a436d66e43542ce3dfc6eb9a7303f3c1addbc60
Pedro Mesmer <pedro_mesmer@hotmail.com>
2024-01-29 01:34:49 -0300

    envio e recebimento de mensagens

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,10 +1,34 @@
 import express from 'express';
+import socketio from 'socket.io';
+import http from 'http';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
-
 app.use(routes);
+app.use(cors());
 
-app.listen(3333, () => {
+const httpServer = http.createServer(app);
+const io = new socketio.Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', socket => {
+  const room = socket.handshake.query?.room;
+  if (room) {
+    socket.join(room);
+  }
+  console.log('new connection: ', socket.id);
+
+  socket.on('message', message => {
+    console.log('message: ', message);
+    if (room) io.to(room).emit('message', { user: socket.id, message });
+  });
+});
+
+httpServer.listen(3333, () => {
   console.log(`🚀 Server started on port 3333`);
 });
